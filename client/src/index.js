4dda60c02c13ec14411dacb449ebaf08d72ac046
Sathvik B <sathvikbk123@gmail.com>
2020-08-08 12:35:59 +0530

    firebase initialise

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+var firebase = require("firebase");
+var firebaseConfig = {
+  apiKey: "AIzaSyBwbOK97yZ-steeSAE88d_2Bru8ApUwKx0",
+  authDomain: "lendahand-16931.firebaseapp.com",
+  databaseURL: "https://lendahand-16931.firebaseio.com",
+  projectId: "lendahand-16931",
+  storageBucket: "lendahand-16931.appspot.com",
+  messagingSenderId: "939016591032",
+  appId: "1:939016591032:web:ed9cdcbc13374d4257c64d",
+  measurementId: "G-1WS7VB7Y8S",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<App />, document.getElementById("root"));
+
 serviceWorker.unregister();
